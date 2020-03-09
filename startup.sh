e5d306dd25848233552b36ef91802f53e250d0f3
Dmitry Antonenko <raerayan@gmail.com>
2020-03-10 00:19:11 +0700

    Stable relese #2

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # If the application folder is empty run expo init, otherwise just start.
-if [ -z "$(ls -A /$@)" ] || [ ! -d /$@ ]; then
+if [ -z "$(ls -A /home/node/$@)" ] || [ ! -d /home/node/$@ ]; then
     expo init $@ --template blank --name $@ && cd ./$@ && expo start
 else
-    cd ./$@ && expo start
+    cd /home/node/$@ && npm start
 fi
